6432e396e65a5f1ab0a387fe641b0c339bd4c134
Ali Servet Donmez <asd@pittle.org>
2016-07-22 16:18:05 +0200

    il primo commit: creato il progetto

---
+++ CorsoMVCSolution/CorsoMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CorsoMVC.Startup))]
+namespace CorsoMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
