a7b512faeb9b2f93220162a7f8b97bc8cfe45807
Ali Servet Donmez <asd@pittle.org>
2016-07-25 09:47:38 +0200

    Modifiche di stile
    * formattato il codice
    * aggiunto config. ReSharper & StyleCop

--- CorsoMVCSolution/CorsoMVC/Controllers/UtentiController.cs
+++ CorsoMVCSolution/CorsoMVC/Controllers/UtentiController.cs
@@ -1,44 +1,16 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="UtentiController.cs" company="">
-//   
-// </copyright>
-// <summary>
-//   The utenti controller.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-
-using System;
-using System.Net;
-using CorsoMVC.Models;
-
-namespace CorsoMVC.Controllers
+﻿namespace CorsoMVC.Controllers
 {
+    using System.Net;
     using System.Web.Mvc;
+    using CorsoMVC.Models;
 
-    /// <summary>
-    /// The utenti controller.
-    /// </summary>
     public class UtentiController : Controller
     {
-        /// <summary>
-        /// The index.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="ActionResult"/>.
-        /// </returns>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public ActionResult Index()
         {
             return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
 
-        /// <summary>
-        /// The semplice testo.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="ActionResult"/>.
-        /// </returns>
         public ActionResult SempliceTesto()
         {
             var result = new ContentResult();
@@ -48,12 +20,6 @@ namespace CorsoMVC.Controllers
             return result;
         }
 
-        /// <summary>
-        /// The html manuale.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="ActionResult"/>.
-        /// </returns>
         public ActionResult HtmlManuale()
         {
             var result = new ContentResult();
@@ -64,29 +30,11 @@ namespace CorsoMVC.Controllers
             return result;
         }
 
-        /// <summary>
-        /// The primo view.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="ActionResult"/>.
-        /// </returns>
         public ActionResult PrimoView()
         {
             return this.View();
         }
 
-        /// <summary>
-        /// The nome variabile.
-        /// </summary>
-        /// <param name="nome">
-        /// The nome.
-        /// </param>
-        /// <param name="cognome">
-        /// The cognome.
-        /// </param>
-        /// <returns>
-        /// The <see cref="ActionResult"/>.
-        /// </returns>
         public ActionResult NomeVariabile(string nome, string cognome)
         {
             object modello = string.Format("{1} {0}", nome, cognome);
@@ -94,15 +42,9 @@ namespace CorsoMVC.Controllers
             return this.View(modello);
         }
 
-        /// <summary>
-        /// The persona.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="ActionResult"/>.
-        /// </returns>
         public ActionResult GetPersona()
         {
-            Persona mario = new Persona();
+            var mario = new Persona();
             mario.Nome = "Mario";
             mario.Cognome = "Rossi";
             mario.Eta = 31;
--- CorsoMVCSolution/CorsoMVC/Models/Persona.cs
+++ CorsoMVCSolution/CorsoMVC/Models/Persona.cs
@@ -1,39 +1,11 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Persona.cs" company="">
-//   
-// </copyright>
-// <summary>
-//   The persona.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-
-
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CorsoMVC.Models
+﻿namespace CorsoMVC.Models
 {
-    /// <summary>
-    /// The persona.
-    /// </summary>
     public class Persona
     {
-        /// <summary>
-        /// Gets or sets the nome.
-        /// </summary>
         public string Nome { get; set; }
 
-        /// <summary>
-        /// Gets or sets the cognome.
-        /// </summary>
         public string Cognome { get; set; }
 
-        /// <summary>
-        /// Gets or sets the eta.
-        /// </summary>
         public int Eta { get; set; }
     }
 }
