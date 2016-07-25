e712e7fd17eca16265c56667fe899e404d400632
Ali Servet Donmez <asd@pittle.org>
2016-07-25 17:11:13 +0200

    esempio admin area

---
+++ CorsoMVCSolution/CorsoMVC/Areas/Admin/Controllers/UtentiController.cs
@@ -0,0 +1,17 @@
+﻿namespace CorsoMVC.Areas.Admin.Controllers
+{
+    using System.Web.Mvc;
+
+    public class UtentiController : Controller
+    {
+        public ActionResult HtmlManuale()
+        {
+            var result = new ContentResult();
+            result.ContentType = "text/html";
+            result.Content =
+                "<html><head><title>Ciao, Mauurizio!</title></head><body><h1>Ciao, Maurizio! (admin)<h1></body></html>";
+
+            return result;
+        }
+    }
+}
